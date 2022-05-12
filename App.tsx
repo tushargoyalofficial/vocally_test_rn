2f455ba8473b1e810c9dfdf16305ecdc3874ae0f
Tushar Goyal <tushar462024@gmail.com>
2022-05-12 17:08:02 +0530

    Map Integration
    Integrate google maps with Dark/Light mode

--- App.tsx
+++ App.tsx
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC, memo, useCallback, useState } from "react";
+import { StyleSheet, View, Dimensions, Pressable } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-export default function App() {
+// Map Styles
+import darkMapStyle from "./src/libs/darkMapStyle";
+import lightMapStyle from "./src/libs/lightMapStyle";
+
+const { height, width } = Dimensions.get("window");
+const LATITUDE = 40.74333; // Korea Town, New York, NY 10001
+const LONGITUDE = -73.99033; // Korea Town, New York, NY 10001
+const LATITUDE_DELTA = 0.28;
+const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+
+const App: FC = () => {
+  const [lightMode, isLightMode] = useState<boolean>(true);
+
+  const updateMapStyle = useCallback(() => {
+    isLightMode(!lightMode);
+  }, [lightMode]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        style={styles.map}
+        zoomControlEnabled
+        maxZoomLevel={20}
+        minZoomLevel={0}
+        loadingEnabled={true}
+        loadingIndicatorColor={"black"}
+        customMapStyle={lightMode ? lightMapStyle : darkMapStyle}
+      />
+      <View style={styles.buttonView}>
+        <Pressable onPress={updateMapStyle} style={styles.iconBtn}>
+          <Ionicons
+            name={lightMode ? "ios-cloudy-night" : "sunny"}
+            size={18}
+            color="white"
+          />
+        </Pressable>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+  },
+  buttonView: {
+    position: "absolute",
+    top: "20%",
+    right: "5%",
+  },
+  iconBtn: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 32,
+    height: 32,
+    borderRadius: 14,
+    backgroundColor: "grey",
   },
 });
+
+export default memo(App);
